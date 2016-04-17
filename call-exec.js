65b1399d5ccd254c1e857d5fccfb405eeadeb5c2
Khải <hvksmr1996@gmail.com>
2016-04-17 13:42:35 +0700

    Update call-exec.js

--- call-exec.js
+++ call-exec.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+console.log(process.argv);
+
 var childprc = require('child_process').spawn('node', ['--es-staging', `${__dirname}/exec.js`, process.argv[2]]);
 
 childprc.on('exit', (code, signal) => {
