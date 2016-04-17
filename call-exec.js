3af321b8a0ce75a76eeaf07763d8018c2fe60c80
Khải <hvksmr1996@gmail.com>
2016-04-17 08:57:15 +0700

    Create call-exec.js

---
+++ call-exec.js
@@ -0,0 +1,12 @@
+
+var childprc = require('child_process').spawn(`node --es-staging ${__dirname}/exec.js process.argv[2]`);
+
+childprc.on('exit', (code, signal) => {
+	if (signal) {
+		throw new Error(`Process terminated, signal: ${signal}`);
+	}
+	process.exit(code);
+});
+
+['stdout', 'stderr']
+	.forEach((stream) => childprc[stream].on('data', (chunk) => process[stream].write(chunk)));
